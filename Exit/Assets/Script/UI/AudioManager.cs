22850b19d92e74a3b74a010ef5faba6c6ff599e5
EmmeForte90 <mauroemme90@gmail.it>
2022-10-25 12:17:51 +0200

    Set5

---
+++ Exit/Assets/Script/UI/AudioManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [Header("Music")]
+    [SerializeField] public AudioSource bgm, dieMusic;
+    [SerializeField] public AudioSource[] soundEffects;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+
+#region Sound 
+    public void PlaySFX(int soundToPlay)
+    {
+        soundEffects[soundToPlay].Stop();
+        soundEffects[soundToPlay].pitch = Random.Range(.9f, 1.1f);
+        soundEffects[soundToPlay].Play();
+    }
+
+    public void StopSFX(int soundToPlay)
+    {
+        soundEffects[soundToPlay].Stop();
+    }
+#endregion
+
+#region Music
+
+    public void stopMusic()
+    {
+        bgm.Stop();
+    }
+     public void playMusic()
+    {
+        bgm.Play();
+    }
+    
+    public void DieMusic()
+    {
+        bgm.Stop();
+        dieMusic.Play();
+    }
+    
+
+    #endregion
+}
---
+++ Exit/Assets/Script/UI/MainMenu.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public string startScene, continueScene;
+    public GameObject opzioni;
+    public GameObject continueButton;
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+         string option = resolutions[i].width + "x" + resolutions[i].height;
+        options.Add(option);
+
+        if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+        {
+            currentResolutionIndex = i;
+        }
+
+        }
+
+        resolutionDropdown.ClearOptions();
+        resolutionDropdown.value = currentResolutionIndex;
+        //resolutionDropdown.RefreshValue();
+        resolutionDropdown.AddOptions(options);
+
+
+
+        if(PlayerPrefs.HasKey(startScene + "_unlocked"))
+        {
+            continueButton.SetActive(true);
+        }
+        else
+        {
+            continueButton.SetActive(false);
+        }
+
+    }
+
+public void SetResolution(int resolutionIndex)
+{
+    Resolution resolution = resolutions[resolutionIndex];
+
+    Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(startScene);
+
+        PlayerPrefs.DeleteAll();
+
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene(continueScene);
+    }
+
+    public void Options()
+    {
+        opzioni.gameObject.SetActive(true);
+    }
+    public void ExitOptions()
+    {
+        opzioni.gameObject.SetActive(false);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting Game");
+
+    }
+
+}
